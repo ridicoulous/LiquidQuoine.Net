99130dccee1bc3f6b04a8ec19cd7448131c87682
Artem Kurianov <artemkuryanov@gmail.com>
2020-08-26 07:46:41 +0000

    interface update

--- LiquidQuoine.Net/Interfaces/ILiquidQuoineSocketClient.cs
+++ LiquidQuoine.Net/Interfaces/ILiquidQuoineSocketClient.cs
@@ -6,9 +6,12 @@ using System.Text;
 namespace LiquidQuoine.Net.Interfaces
 {
     public interface ILiquidQuoineSocketClient
-    {       
+    {
+
         void SubscribeToOrderBookSide(string symbol, OrderSide side, Action<List<LiquidQuoineOrderBookEntry>, OrderSide, string> onData);
         void SubscribeToUserExecutions(string symbol, Action<LiquidQuoineExecution,string> onData);
         void SubscribeToExecutions(string symbol, Action<LiquidQuoineExecution, string> onData);
+        void SubscribeToUserOrdersUpdate(Action<LiquidQuoinePlacedOrder> onData, string[] fundingCurrensies = null);
+
     }
 }
